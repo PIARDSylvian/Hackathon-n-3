3872659f44dcee95419fcdb45ca867350524fe85
PIARDSylvian <piard.sylvian@gmail.com>
2016-06-16 20:26:48 +0200

    profil modif name email

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse; 
 
 class ProfileController extends Controller
 {
@@ -13,14 +14,28 @@ class ProfileController extends Controller
      */
     public function profileAction(Request $request)
     {
-    	$em = $this->getDoctrine()->getManager();
-    	$users = $this->container->get('security.context')->getToken()->getUser();
- 
-    	$user = $em->getRepository("AppBundle:user")->findOneById($users->getId());
-
-
-    	 // var_dump($user);exit;
+    	$user = $this->getUser();
 
         return $this->render('AppBundle::profile.html.twig', array("datauser" => $user));
+    }
+
+    public function editAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        $username = $request->request->get('username');
+        $email = $request->request->get('email');
+
+        $user->setUsername($username);
+        $user->setEmail($email);
+        
+
+        $em->persist($user);
+        $em->flush();
+
+        $url = $this -> generateUrl('profile');
+        $response = new RedirectResponse($url);
+        return $response;
     }
 }
